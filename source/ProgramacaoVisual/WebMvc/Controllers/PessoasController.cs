a8b8dffe403b109f74ab9adcf8ad3cfcb5fca7b8
Geruza <geruzagsantos@gmail.com>
2018-03-14 01:25:39 +0000

    Aula04

---
+++ source/ProgramacaoVisual/WebMvc/Controllers/PessoasController.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebMvc.Models;
+
+namespace WebMvc.Controllers
+{
+    public class PessoasController : Controller
+    {
+        private static IList<Pessoa> pessoaList = new List<Pessoa>
+        {
+            new Pessoa { Id = 1, Nome = "Joao"},
+            new Pessoa { Id = 2, Nome = "Juao"},
+            new Pessoa { Id = 3, Nome = "John"}
+        };
+        //Actions
+        //localhost:5000/Pessoas
+        //localhost:5000/Pessoas/Index
+        public IActionResult Index()
+        {
+            //Busca dos dados
+            //Transforma dados em dados de visualização
+            //Visualização de dados
+            var viewModel = new PessoaViewModel
+            {
+            Items = pessoaList
+            };
+        //visualização dos dados
+        return View(viewModel);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+
+    public IActionResult Create ([Bind("Nome")]Pessoa newPessoa)
+    {
+        if (!ModelState.IsValid){
+            return View(newPessoa);
+        }
+    newPessoa.Id = pessoaList.Max(p=> p.Id) + 1;
+    pessoaList.Add(newPessoa);
+    return RedirectToAction(nameof(Index)); 
+    }
+    
+    }
+}
---
+++ source/ProgramacaoVisual/WebMvc/Models/PessoaViewModel.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+
+namespace WebMvc.Models
+{
+    public class PessoaViewModel
+    {
+
+        //CamelCase
+        //pascalCase
+        //snake_case
+
+        public IList<Pessoa> Items { get; set; }
+    }
+}
